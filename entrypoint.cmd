1ac8a89902e0a09b0e1b2925ef13129f81839877
Youyuan Wu <youyuanwu@outlook.com>
2020-12-24 14:43:55 -0800

    save

--- entrypoint.cmd
+++ entrypoint.cmd
@@ -1,27 +1,30 @@
 @echo off
 
-REM Create and start DB instance
-set DB_INSTANCE_NAME=MSSQLLocalDB
-SqlLocalDB.exe create %DB_INSTANCE_NAME% -s
-
-set DB_DATA_DIR=C:\data\%DB_INSTANCE_NAME%
+set DB_INSTANCE_NAME=MSSQLLocalDb
+set DB_DATA_DIR=C:\data
 set DB_NAME=AzureStorageEmulatorDb510
 
-if not exist "%DB_DATA_DIR%" mkdir %DB_DATA_DIR%
 REM In win server 2019 sqlcmd has permission issue creating files without change permission
-icacls "%DB_DATA_DIR%" /grant Everyone:M
+echo [entrypoint] Change C:\data permission.
+icacls "C:\data" /grant Everyone:M
+if not exist "%DB_DATA_DIR%" mkdir %DB_DATA_DIR%
 
-REM If persistent DB data exists restore it, else create a new DB
+echo [entrypoint] Create and start DB instance %DB_INSTANCE_NAME%.
+SqlLocalDB.exe create %DB_INSTANCE_NAME% -s
+
+REM If persistent DB data exists restore it, else create DB with fixed file location
 if exist %DB_DATA_DIR%/%DB_NAME%.mdf (
-  echo [debug] previous DB file found. Restoring DB.
+  echo [entrypoint] Previous DB file found. Restoring DB.
   sqlcmd.exe -S "(localdb)\%DB_INSTANCE_NAME%" -i "C:\restore.sql"
 ) else (
-  echo [debug] previous DB file not found. Creating New DB.  
+  echo [entrypoint] Previous DB file not found. Creating New DB.  
   sqlcmd.exe -S "(localdb)\%DB_INSTANCE_NAME%" -i "C:\init.sql"
 )
 
-REM Init and start Emulator 
+REM Init and start Emulator
+echo [entrypoint] Init and start AzureStorageEmulator. 
 AzureStorageEmulator.exe init
 AzureStorageEmulator.exe start
 
+echo [entrypoint] AzureStorageEmulator is started and ready.
 call %*
